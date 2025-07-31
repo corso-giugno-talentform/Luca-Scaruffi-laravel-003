06aef0763b129e3474b9e5c0a255f95df6a48ed5
Luca Scaruffi <lucascaruffi@MacBook-Pro-di-Luca.local>
2025-07-31 18:21:04 +0200

    modifica file

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,6 @@
 <footer class="bg-light text-dark text-center py-5 mt-5 border-top shadow-sm">
     <div class="container">
-        <p class="mb-0 text-muted">&copy; {{ date('Y') }} [Il Tuo Nome] - Libreria. Tutti i diritti riservati.</p>
-        <div class="mt-3">
-            <a href="https://www.linkedin.com/in/tuo-profilo" target="_blank" class="text-primary mx-3 social-icon"><i
-                    class="bi bi-linkedin fs-3"></i></a>
-            <a href="https://github.com/tuo-profilo" target="_blank" class="text-dark mx-3 social-icon"><i
-                    class="bi bi-github fs-3"></i></a>
-        </div>
+        <p class="mb-0 text-muted">&copy; {{ date('Y') }} Libreria | Tutti i diritti riservati.</p>
+
     </div>
 </footer>
